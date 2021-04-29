ecad9fc3890fa49467964119faadb92a2eaf43d5
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2021-04-29 10:02:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activey = 'go to the office') {
+    console.log(`This Monday, I will ${activey}.`);
+    return `This Monday, I will ${activey}.`;
+}
+mondayWork();
+
+function wrapAdjective(first = '*') {
+    return function(adject = 'special') {
+        return `You are ${first}${adject}${first}!`
+    }
+}
